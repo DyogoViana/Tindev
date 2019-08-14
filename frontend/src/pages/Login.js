7913e59c0c25a07aa4db45ce1e3156cff082dd27
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-08-14 14:34:38 -0300

    Redirecionado a rota ao enviar pelo form.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,12 +7,27 @@ import React, { useState } from "react";
 import "./Login.css"; // Não precisa colocar o nome antes, já que só quero o arquivo. 
 import logoTindev from "../assets/logoTindev.svg";
 
-export default function Login() {
+export default function Login({ history }) {
+    const [userName, setUserName] = useState(""); // 'setUserName' para modificar e 'userName' para acessar.
+
+    // Inicia quando o usuário enviar um submit no form.
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        console.log(userName);
+
+        history.push("/main"); // Redireciona para a rota escolhida.
+    }
+
     return(
         <div className="login-container">
-            <form>
+            <form onSubmit= { handleSubmit }>
                 <img src={ logoTindev } alt="Logo Tindev"/>
-                <input placeholder="Digite seu usuário do GitHub."/>
+                <input 
+                    placeholder="Digite seu usuário do GitHub."
+                    value={ userName }
+                    onChange={ event => setUserName(event.target.value) } // Toda mudança que houver no 'input'.
+                />
                 <button type="submit">Enviar</button>
             </form>
         </div>
